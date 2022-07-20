3ce74a0e013dad5a3de70588c9f7a3a49cf99f6f
Users879 <109139911+Users879@users.noreply.github.com>
2022-07-20 17:07:27 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите первое число");
-        int a= scanner.nextInt();
+        float a= scanner.nextInt();
         System.out.println(a+"первое число");
         System.out.println("Введите второе число");
-        int b=scanner.nextInt();
-        int c=a+b;
+        float b=scanner.nextInt();
+        float c=a/b;
         System.out.println("Ответ: " + c);
 
     }
